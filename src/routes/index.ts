e54ae6e91f6d50f6da9cfefe48a5a26d21686beb
Andre Macedo <dee.xd@hotmail.com>
2020-10-20 14:29:31 -0300

    feat: add swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import { Request, Response, Router } from 'express';
 
+import { swaggerSpec } from '../swagger';
+
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
     res.send('Service 1.0.0');
+});
+
+router.get('/api-docs', (req: Request, res: Response) => {
+    res.send(swaggerSpec);
 });
 
 export default router;
---
+++ src/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerJSDoc, { SwaggerDefinition, Options } from 'swagger-jsdoc';
+
+const swaggerDefinition = {
+    info: {
+        title: 'API Generic Service',
+        version: '1.0.0',
+    },
+    host: process.env.HOST || 'localhost:3000',
+    basePath: '/',
+} as SwaggerDefinition;
+
+const options = {
+    swaggerDefinition,
+    apis: [
+        './dist/routes/index.js'
+    ],
+} as Options;
+
+const swaggerSpec = swaggerJSDoc(options) as SwaggerDefinition;
+
+export { swaggerSpec };
