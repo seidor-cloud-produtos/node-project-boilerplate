1ec37fe71d9dd50bd82d314caeb29f270d132e50
Andre Macedo <andre.macedo@seidor.com.br>
2020-10-22 17:02:24 -0300

    chore: add mongo connection

--- src/database/connection/connection.ts
+++ src/database/connection/connection.ts
@@ -1,17 +1,29 @@
 import path from 'path';
 import dotenv from 'dotenv';
-import { createConnection, Connection } from 'typeorm';
+import { createConnections, Connection } from 'typeorm';
 
 dotenv.config();
 
-export default async (): Promise<Connection> => {
-    return createConnection({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT!,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [`${path.resolve(__dirname, '../entities')}/*.{ts,js}`],
-    });
+export default async (): Promise<Connection[]> => {
+    return createConnections([
+        {
+            name: 'default',
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT!,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [`${path.resolve(__dirname, '../entities')}/*.{ts,js}`],
+        },
+        {
+            name: 'mongo',
+            type: 'mongodb',
+            host: process.env.MONGO_HOST,
+            port: +process.env.MONGO_PORT!,
+            database: process.env.MONGO_DBNAME,
+            useUnifiedTopology: true,
+            entities: [`${path.resolve(__dirname, '../schemas')}/*.{ts,js}`],
+        },
+    ]);
 };
