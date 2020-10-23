4d344fbed7cf6f05f29b707fd26463ac98434991
joao.rezende <joaorabello99@gmail.com>
2020-10-23 16:56:06 -0300

    feat: add getValidData to get properties of request

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,17 +1,23 @@
 import { Request, Response, Router } from 'express';
 import { container } from 'tsyringe';
 import BookController from '../controllers/book';
+import * as bookValidator from '../utils/book/validators';
+import { getValidData } from '../utils/validation/validatonHandler';
 
 const router = Router();
 
-router.post('/', async (req: Request, res: Response) => {
-    const book = req.body;
+router.post(
+    '/',
+    bookValidator.createValidators,
+    async (req: Request, res: Response) => {
+        const { body } = getValidData(req);
 
-    const bookController = container.resolve(BookController);
+        const bookController = container.resolve(BookController);
 
-    const book_response = await bookController.create(book);
+        const book_response = await bookController.create(body);
 
-    return res.status(201).send(book_response);
-});
+        return res.status(201).send(book_response);
+    },
+);
 
 export default router;
