6337c85fbcc1aec7ca7613790673c48becc6b94c
joao.rezende <joaorabello99@gmail.com>
2020-10-23 13:51:29 -0300

    feat: create book repository

--- src/database/connection/connection.ts
+++ src/database/connection/connection.ts
@@ -1,10 +1,21 @@
 import path from 'path';
 import dotenv from 'dotenv';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { createConnections, Connection } from 'typeorm';
 
 dotenv.config();
 
 export default async (): Promise<Connection[]> => {
+    let mongo_port = Number(process.env.MONGO_PORT);
+    let mongo_database = process.env.MONGO_DBNAME;
+
+    if (process.env.CURRENT_ENVIROMENT === 'DEV') {
+        const mongo_data = new MongoMemoryServer();
+
+        mongo_port = await mongo_data.getPort();
+        mongo_database = await mongo_data.getDbName();
+    }
+
     return createConnections([
         {
             name: 'default',
@@ -20,8 +31,8 @@ export default async (): Promise<Connection[]> => {
             name: 'mongo',
             type: 'mongodb',
             host: process.env.MONGO_HOST,
-            port: +process.env.MONGO_PORT!,
-            database: process.env.MONGO_DBNAME,
+            port: +mongo_port!,
+            database: mongo_database,
             useUnifiedTopology: true,
             entities: [`${path.resolve(__dirname, '../schemas')}/*.{ts,js}`],
         },
