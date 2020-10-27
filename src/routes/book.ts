d77ad91680b727e86228af37e99f099ca1e0a21e
joao.rezende <joaorabello99@gmail.com>
2020-10-27 16:03:50 -0300

    feat: alter validate to middleware

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,23 +1,19 @@
 import { Request, Response, Router } from 'express';
 import { container } from 'tsyringe';
 import BookController from '../controllers/book';
-import * as bookValidator from '../utils/book/validators';
+import validateUserDataMiddleware from '../utils/book/middleware/validateBookData';
 import { getValidData } from '../utils/validation/validatonHandler';
 
 const router = Router();
 
-router.post(
-    '/',
-    bookValidator.createValidators,
-    async (req: Request, res: Response) => {
-        const { body } = getValidData(req);
+router.post('/', validateUserDataMiddleware, async (req: Request, res: Response) => {
+    const { body } = getValidData(req);
 
-        const bookController = container.resolve(BookController);
+    const bookController = container.resolve(BookController);
 
-        const book_response = await bookController.create(body);
+    const book_response = await bookController.create(body);
 
-        return res.status(201).send(book_response);
-    },
-);
+    return res.status(201).send(book_response);
+});
 
 export default router;
