6fd65a05530f6338bda1845790a18bc395cfea0a
Andre Macedo <dee.xd@hotmail.com>
2020-11-16 17:39:18 -0300

    refactor: book route to use new generic validator middleware

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,18 +1,24 @@
 import { Request, Response, Router } from 'express';
 import { container } from 'tsyringe';
+
 import BookController from '../controllers/book';
-import validateUserDataMiddleware from '../utils/book/middleware/validateBookData';
+import { bookSchema } from '../utils/book/validators';
+import validatorMiddleware from '../utils/middleware/validator';
 
 const router = Router();
 
-router.post('/', validateUserDataMiddleware, async (req: Request, res: Response) => {
-    const book_data = req.body;
+router.post(
+    '/',
+    validatorMiddleware({ body: bookSchema }),
+    async (req: Request, res: Response) => {
+        const book_data = req.body;
 
-    const bookController = container.resolve(BookController);
+        const bookController = container.resolve(BookController);
 
-    const book_response = await bookController.create(book_data);
+        const book_response = await bookController.create(book_data);
 
-    return res.status(201).send(book_response);
-});
+        return res.status(201).send(book_response);
+    },
+);
 
 export default router;
--- src/utils/book/validators/index.ts
+++ src/utils/book/validators/index.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const userSchema = yup.object().shape({
+export const bookSchema = yup.object().shape({
     author: yup.string().strict(true).required('The property author is required'),
     genre: yup.string().strict(true).required('The property genre is required'),
     name: yup.string().strict(true).required('The property name is required'),
