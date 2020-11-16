5b6980c907003cffac77262ecbdcfdc16cea97d2
PaabloMelo <pablo.melo@tangotech.com.br>
2020-11-16 17:12:59 -0300

    refactor: create test service

---
+++ src/tests/repositories/fakes/FakeBookRepository.ts
@@ -0,0 +1,17 @@
+import IBookRepository from '../../../interfaces/repositories/IBook';
+import Book from '../../../database/schemas/Book';
+import { BookInterface } from '../../../interfaces/book';
+
+export default class BookRepository implements IBookRepository {
+    private books: BookInterface[] = [];
+
+    public async createAndSave(bookData: BookInterface): Promise<Book> {
+        const book = new Book();
+
+        Object.assign(book, bookData);
+
+        this.books.push(book);
+
+        return book;
+    }
+}
