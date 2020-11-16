ab3f60bb173f93794f633d17f611e583f5dab5ba
Andre Macedo <dee.xd@hotmail.com>
2020-11-16 17:38:38 -0300

    feat: add generic validator middleware

---
+++ src/utils/middleware/validator.ts
@@ -0,0 +1,18 @@
+import { Response, Request, NextFunction, RequestHandler } from 'express';
+import * as yup from 'yup';
+
+import { validateSchemaData } from '../validation/validationError';
+
+export default function validatorMiddleware(schemas: {
+    body?: yup.ObjectSchema;
+    headers?: yup.ObjectSchema;
+    query?: yup.ObjectSchema;
+}): RequestHandler {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        if (schemas.headers) await validateSchemaData(req.headers, schemas.headers);
+        if (schemas.query) await validateSchemaData(req.query, schemas.query);
+        if (schemas.body) await validateSchemaData(req.body, schemas.body);
+
+        next();
+    };
+}
