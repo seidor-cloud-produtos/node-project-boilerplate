1cd65378155166f561f4a8e7cac8429bec73bc01
PaabloMelo <pablo.melo@tangotech.com.br>
2020-11-17 12:27:53 -0300

    chore: add container in constructor class

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -4,12 +4,16 @@ import { container } from 'tsyringe';
 import BookService from '../services/book';
 
 export default class BookController {
+    bookService: BookService;
+
+    constructor() {
+        this.bookService = container.resolve(BookService);
+    }
+
     public async create(request: Request, response: Response): Promise<Response> {
         const book_data = request.body;
 
-        const bookService = container.resolve(BookService);
-
-        const book_response = await bookService.create(book_data);
+        const book_response = await this.bookService.create(book_data);
 
         return response.status(201).json(book_response);
     }
