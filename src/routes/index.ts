09308c102756b98637c4207e8c075f28f8e1889f
Andre Macedo <andre.macedo@seidor.com.br>
2020-11-17 18:13:54 -0300

    refactor: remove out of date files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Request, Response, Router } from 'express';
 
 import { swaggerSpec } from '../swagger';
-import apiUser from './user';
 import apiBook from './book';
 
 const router = Router();
@@ -14,7 +13,6 @@ router.get('/api-docs', (req: Request, res: Response) => {
     res.send(swaggerSpec);
 });
 
-router.use('/api/user', apiUser);
 router.use('/api/book', apiBook);
 
 export default router;
