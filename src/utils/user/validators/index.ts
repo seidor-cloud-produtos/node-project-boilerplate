b875894b10d1c33d3d1019389cf6fa9facda2d90
Renato Assis <renato.assis@seidor.com.br>
2021-02-02 10:53:00 -0300

    refactor: user schema validator

--- src/utils/user/validators/index.ts
+++ src/utils/user/validators/index.ts
@@ -1,22 +1,11 @@
-import { body, query, param } from 'express-validator';
+import * as yup from 'yup';
 
-export const idParamValidator = [param('id').isUUID()];
-
-export const createValidators = [
-    body('name').isString(),
-    body('surname').isString(),
-    body('age').isNumeric(),
-    body('email').isEmail().optional(),
-];
-
-export const updateValidators = [
-    body('name').isString().optional(),
-    body('surname').isString().optional(),
-    body('age').isNumeric().optional(),
-    body('email').isEmail().optional(),
-].concat(idParamValidator);
-
-export const getallValidator = [
-    query('size').isNumeric().optional(),
-    query('page').isNumeric().optional(),
-];
+export const createUserSchema = yup.object().shape({
+    name: yup.string().strict(true).required('The property name is required'),
+    surname: yup.string().strict(true).required('The property surname is required'),
+    email: yup
+        .string()
+        .email()
+        .strict(true)
+        .required('The property email is required'),
+});
